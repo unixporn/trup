a2f8192080f6d8ff7de0d9c141e0dea99b992457
mlvzk <mlvzk@protonmail.com>
2020-05-19 16:25:44 +0200

    add better success message for desc

--- command/desc.go
+++ command/desc.go
@@ -52,5 +52,5 @@ func desc(ctx *Context, args []string) {
 		return
 	}
 
-	ctx.Reply("Your description has been set successfully.")
+	ctx.Reply("Your description has been set successfully. You can see it with .fetch")
 }
