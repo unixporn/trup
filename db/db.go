a0de752966e67d6b6f55c3620cfe643aaf64ecd9
Alejandro Sior <aho@sior.be>
2020-05-25 17:56:21 +0200

    Implemented optional database

--- db/db.go
+++ db/db.go
@@ -2,16 +2,31 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"os"
 )
 
-var db *pgxpool.Pool
+var (
+	db         *pgxpool.Pool
+	databaseOk = false
+)
+
+func DatabaseUsable() bool {
+	return databaseOk
+}
 
 func init() {
-	conn, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		panic(err)
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		fmt.Println("Database disabled")
+		return
 	}
+	conn, err := pgxpool.Connect(context.Background(), databaseUrl)
+	if err != nil {
+		fmt.Printf("Unable to connect to the database, the database features will be disabled:\n%s\n", err)
+		return
+	}
+	databaseOk = true
 	db = conn
 }
