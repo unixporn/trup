238e24b0fc2c38af9139d5b10e463f4ec756e0c5
Tanish2002 <tanishkhare@gmail.com>
2020-09-02 19:17:50 +0530

    made the help command cleaner

--- command/help.go
+++ command/help.go
@@ -1,25 +1,52 @@
 package command
 
-import "strings"
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
 func Help(ctx *Context, args []string) {
-	var text strings.Builder
 
+	const inline = true
+	embed := discordgo.MessageEmbed{
+		Title:  "Help",
+		Fields: []*discordgo.MessageEmbedField{},
+		Color:  ctx.Session.State.UserColor(ctx.Message.Author.ID, ctx.Message.ChannelID),
+	}
 	isCallerModerator := ctx.isModerator()
 	for name, cmd := range Commands {
 		if cmd.ModeratorOnly && !isCallerModerator {
 			continue
 		}
 
-		text.WriteString("**" + name + "**")
 		if cmd.Usage != "" {
-			text.WriteString(" - Usage: " + cmd.Usage)
+			if cmd.Help != "" {
+				embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+					name + " - Usage " + cmd.Usage,
+					cmd.Help,
+					inline,
+				})
+			} else {
+				embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+					name + " - Usage " + cmd.Usage,
+					"\u200b",
+					inline,
+				})
+			}
+		} else {
+			if cmd.Help != "" {
+				embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+					name,
+					cmd.Help,
+					inline,
+				})
+			} else {
+				embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+					name,
+					"\u200b",
+					inline,
+				})
+			}
 		}
-		if cmd.Help != "" {
-			text.WriteString("\n> " + cmd.Help)
-		}
-		text.WriteByte('\n')
 	}
-
-	ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, "\n"+text.String())
+	ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, &embed)
 }
