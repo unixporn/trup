0656f8c9e8d5ec34eeaa430ad8d655ada0350fee
Tanish2002 <tanishkhare@gmail.com>
2020-09-03 17:45:46 +0530

    some suggested changes + role colors

--- command/info.go
+++ command/info.go
@@ -1,9 +1,9 @@
 package command
 
 import (
+	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/dustin/go-humanize"
-	"log"
 	"strings"
 )
 
@@ -14,7 +14,8 @@ const (
 
 // struct for getting user roles
 type discordRole struct {
-	Name string `json:"name"`
+	ID    string `json:"id"`
+	Color string `json:"color"`
 }
 
 func info(ctx *Context, args []string) {
@@ -39,15 +40,18 @@ func info(ctx *Context, args []string) {
 		ctx.ReportError("Failed to find member info.", err)
 		return
 	}
-	//dates
+	// dates
+
 	accountCreateDate, err := discordgo.SnowflakeTimestamp(user.ID)
 	if err != nil {
-		log.Println("Failed to find Account Creation Date. ", err)
+		ctx.ReportError("Failed to find Account Creation Date. ", err)
+		return
 	}
 
 	joinDate, err := member.JoinedAt.Parse()
 	if err != nil {
-		log.Println("Failed to find Server Join Date. ", err)
+		ctx.ReportError("Failed to find Server Join Date. ", err)
+		return
 	}
 	// no error handling here because for Non-Boosters premiumDate would always give error
 	premiumDate, _ := member.PremiumSince.Parse()
@@ -57,51 +61,62 @@ func info(ctx *Context, args []string) {
 	for _, role := range member.Roles {
 		role, err := ctx.Session.State.Role(ctx.Message.GuildID, role)
 		if err != nil {
-			log.Println("Failed to get user roles from discord server.")
+			ctx.ReportError("Failed to get user roles from discord server.", err)
 			return
 		}
+
+		color := fmt.Sprintf("%x", (role.Color))
+
 		r := discordRole{
-			Name: role.Name,
+			ID:    role.Mention(),
+			Color: color,
 		}
 		discordRoles = append(discordRoles, r)
 	}
-	//roles of user
+	// roles of user
 	var userRoles []string
+	var userColors []string
+	var roles []string
 	for _, r := range discordRoles {
-		userRoles = append(userRoles, r.Name)
+		userRoles = append(userRoles, r.ID)
+		userColors = append(userColors, r.Color)
 	}
-
+	for i := range userRoles {
+		// check for no color
+		if userColors[i] == "0" {
+			roles = append(roles, userRoles[i])
+		} else {
+			roles = append(roles, userRoles[i]+"(#"+userColors[i]+")")
+		}
+	}
 	const inline = false
 	embed := discordgo.MessageEmbed{
 		Title:  user.Username + "#" + user.Discriminator,
 		Fields: []*discordgo.MessageEmbedField{},
 	}
 
-	//embed Thumbnail
+	// embed Thumbnail
 	embed.Thumbnail = &discordgo.MessageEmbedThumbnail{
 		URL: user.AvatarURL("128"),
 	}
 
-	//embed color
+	// embed color
 	embed.Color = ctx.Session.State.UserColor(user.ID, ctx.Message.ChannelID)
 
-	//Embed Fields :-
-	if accountCreateDate.String() != "" {
-		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-			"Account Creation Date",
-			accountCreateDate.UTC().Format("2006-01-02 15:04") + " (" + humanize.Time((accountCreateDate)) + ")",
-			inline,
-		})
-	}
+	// Embed Fields :-
+	embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+		"Account Creation Date",
+		accountCreateDate.UTC().Format("2006-01-02 15:04") + " (" + humanize.Time((accountCreateDate)) + ")",
+		inline,
+	})
 
-	if joinDate.String() != "" {
-		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-			"Join Date",
-			joinDate.UTC().Format("2006-01-02 15:04") + " (" + humanize.Time((joinDate)) + ")",
-			inline,
-		})
-	}
-	if premiumDate.String() != "0001-01-01 00:00:00 +0000 UTC" {
+	embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+		"Join Date",
+		joinDate.UTC().Format("2006-01-02 15:04") + " (" + humanize.Time((joinDate)) + ")",
+		inline,
+	})
+
+	if premiumDate.IsZero() != true {
 		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
 			"Booster Since",
 			premiumDate.UTC().Format("2006-01-02 15:04") + " (" + humanize.Time((premiumDate)) + ")",
@@ -111,7 +126,7 @@ func info(ctx *Context, args []string) {
 	if strings.Join(userRoles, ", ") != "" {
 		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
 			"Roles",
-			strings.Join(userRoles, ", "),
+			strings.Join(roles, ", "),
 			inline,
 		})
 	}
