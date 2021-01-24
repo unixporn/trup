b628baadbc3818ea99f4b2fe19e27f1630756557
star kiwmi <40295114+6gk@users.noreply.github.com>
2021-01-24 18:54:30 +1000

    fix not checking for errors because linters suck

--- command/say.go
+++ command/say.go
@@ -8,6 +8,9 @@ const sayUsage = "say [text] - makes the bot say [text]"
 
 func say(ctx *Context, args []string) {
 	reply := strings.Join(args[1:], " ")
-	ctx.Session.ChannelMessageDelete(ctx.Message.ChannelID, ctx.Message.ID)
+	err := ctx.Session.ChannelMessageDelete(ctx.Message.ChannelID, ctx.Message.ID)
+	if err != nil {
+		return
+	}
 	ctx.Reply(reply)
 }
