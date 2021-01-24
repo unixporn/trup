bc606319e843ea51cde3955a9395e85e14980a98
star kiwmi <40295114+6gk@users.noreply.github.com>
2021-01-24 18:29:56 +1000

    fix some linting errors (but not error checking)

--- command/say.go
+++ command/say.go
@@ -2,7 +2,6 @@ package command
 
 import (
 	"strings"
-	"fmt"
 )
 
 const sayUsage = "say [text] - makes the bot say [text]"
@@ -10,5 +9,5 @@ const sayUsage = "say [text] - makes the bot say [text]"
 func say(ctx *Context, args []string) {
 	reply := strings.Join(args[1:], " ")
 	ctx.Session.ChannelMessageDelete(ctx.Message.ChannelID, ctx.Message.ID)
-	ctx.Reply(fmt.Sprintf("%s", reply))
+	ctx.Reply(reply)
 }
