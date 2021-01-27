69418f43615654470290c40a323a753e388a363e
Tanish2002 <tanishkhare@gmail.com>
2021-01-28 01:20:03 +0530

    Add support for !top[item]

--- command/top.go
+++ command/top.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -10,26 +11,55 @@ import (
 )
 
 const (
-	topHelp = "Displays the most used distro, terminal, etc."
+	topHelp  = "Displays the most used distro, terminal, etc."
+	topUsage = "`!top` or `!top [Distro OR DeWm OR Terminal etc.]`"
 )
 
 func top(ctx *Context, args []string) {
-	topFields, err := db.TopSysinfoFields()
-	if err != nil {
-		ctx.ReportError("Failed to get top fields", err)
-		return
-	}
+	if len(args) == 2 {
+		topFields, err := db.TopArginfoFields(args[1])
+		if err != nil {
+			ctx.ReportError("Failed to get top fields", err)
+			return
+		}
+		if len(topFields) == 0 {
+			ctx.Reply("Arguement not found")
+			return
+		}
+		var description strings.Builder
+		i := 1
+		for _, field := range topFields {
+			description.WriteString(fmt.Sprintf("%d. %s (%s%%)\n", i, field.Name, strconv.Itoa(field.Percentage)))
+			i += 1
+		}
 
-	var description strings.Builder
-	for _, field := range topFields {
-		description.WriteString("**" + field.Field + "**: " + field.Name + " (" + strconv.Itoa(field.Percentage) + "%)\n")
-	}
+		_, err = ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, &discordgo.MessageEmbed{
+			Title:       "Top " + args[1],
+			Description: description.String(),
+		})
+		if err != nil {
+			log.Println("Failed on ChannelMessageSendEmbed in top", err)
+		}
+	} else if len(args) == 1 {
+		topFields, err := db.TopSysinfoFields()
+		if err != nil {
+			ctx.ReportError("Failed to get top fields", err)
+			return
+		}
 
-	_, err = ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, &discordgo.MessageEmbed{
-		Title:       "Top Fields",
-		Description: description.String(),
-	})
-	if err != nil {
-		log.Println("Failed on ChannelMessageSendEmbed in top", err)
+		var description strings.Builder
+		for _, field := range topFields {
+			description.WriteString("**" + field.Field + "**: " + field.Name + " (" + strconv.Itoa(field.Percentage) + "%)\n")
+		}
+
+		_, err = ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, &discordgo.MessageEmbed{
+			Title:       "Top Fields",
+			Description: description.String(),
+		})
+		if err != nil {
+			log.Println("Failed on ChannelMessageSendEmbed in top", err)
+		}
+	} else {
+		ctx.Reply("Usage: " + topUsage)
 	}
 }
