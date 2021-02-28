57feda989509d419cb89d59886a0ef92204a37be
Tanish2002 <tanishkhare@gmail.com>
2021-03-01 03:50:12 +0530

    use m.JoinedAt instead of time.Now()

--- eventhandler/memberjoin.go
+++ eventhandler/memberjoin.go
@@ -6,6 +6,7 @@ import (
 	"runtime/debug"
 	"trup/ctx"
 	"trup/db"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/dustin/go-humanize"
@@ -19,6 +20,10 @@ func MemberJoin(ctx *ctx.Context, m *discordgo.GuildMemberAdd) {
 	}()
 
 	accountCreateDate, _ := discordgo.SnowflakeTimestamp(m.User.ID)
+	joinDate, err := m.JoinedAt.Parse()
+	if err != nil {
+		joinDate = time.Now().UTC()
+	}
 	embed := discordgo.MessageEmbed{
 		Author: &discordgo.MessageEmbedAuthor{
 			IconURL: m.User.AvatarURL("64"),
@@ -28,16 +33,16 @@ func MemberJoin(ctx *ctx.Context, m *discordgo.GuildMemberAdd) {
 		Fields: []*discordgo.MessageEmbedField{
 			{
 				Name:  "Account Creation Date",
-				Value: accountCreateDate.UTC().Format("2006-01-02 15:04") + " (" + humanize.Time(accountCreateDate) + ")",
+				Value: accountCreateDate.UTC().String() + " (" + humanize.Time(accountCreateDate) + ")",
 			},
 			{
 				Name:  "Join Date",
-				Value: accountCreateDate.UTC().Format("2006-01-02 15:04"),
+				Value: joinDate.String(),
 			},
 		},
 	}
 
-	_, err := ctx.Session.ChannelMessageSendEmbed(ctx.Env.ChannelBotTraffic, &embed)
+	_, err = ctx.Session.ChannelMessageSendEmbed(ctx.Env.ChannelBotTraffic, &embed)
 	if err != nil {
 		log.Printf("Failed to send embed to channel %s of user(%s) join. Error: %s\n", ctx.Env.ChannelBotTraffic, m.User.ID, err)
 	}
